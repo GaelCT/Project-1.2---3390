dbefa79eb68e75bc53d110c6bd50e2c9cbcc4e24
Gael <gaelcarrillo995@gmail.com>
2025-09-20 18:38:35 -0700

    have finally finished the boring part. I need to add more colors and make it cool

--- lib/main.dart
+++ lib/main.dart
@@ -1,274 +1,102 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  //int _counter = 0;
-
-  /*void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }*/
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple.shade400,
-          brightness: Brightness.dark,
-        ),
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: Colors.blueGrey,
-            child: Center(
-              child: DataTable(
-                border: TableBorder.all(color: Colors.white),
-                decoration: BoxDecoration(shape: BoxShape.rectangle),
-                headingTextStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                columns: [
-                  DataColumn(
-                    label: ConstrainedBox(
-                      constraints: BoxConstraints(minWidth: 120),
-                      child: Text("Habit", textAlign: TextAlign.center),
-                    ),
-                  ),
-                  DataColumn(
-                    label: ConstrainedBox(
-                      constraints: BoxConstraints(minWidth: 150),
-                      child: Text("Date", textAlign: TextAlign.center),
-                    ),
-                  ),
-                ],
-                rows: [
-                  DataRow(
-                    cells: [
-                      //DataCell(TextField()), this for user input
-                      //DataCell(DateTime.now()), This for getting the current time
-                      DataCell(Text("HI 2")),
-                      DataCell(
-                        Text(
-                          "hi",
-                          textAlign: TextAlign.center,
-                          textScaler: TextScaler.linear(1.5),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 290,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  //controller: TextEditingController(text: ),
-                  decoration: InputDecoration(fillColor: Colors.grey,
-                  hintText: "Enter Habit!",
-                  labelStyle: TextStyle(backgroundColor: Colors.black)
-                  ),
-                  keyboardType: TextInputType.text,
-
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 200,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FilledButton(
-                  onPressed: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => Page2()),
-                    //);
-                  },
-                  child: Text("Add", textScaler: TextScaler.linear(1.7),),
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(100, 55),
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 200,
-            left: 250,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FilledButton(
-                  onPressed: () {
-                    //Navigator.pop(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => Page2()),
-                    //);
-                  },
-                  child: Text("Save", textScaler: TextScaler.linear(1.7)),
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(100, 55),
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 200,
-            left: 0,
-            right: 250,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FilledButton(
-                  onPressed: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => Page2()),
-                    //);
-                  },
-                  child: Text("Load", textScaler: TextScaler.linear(1.7), ),
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(100, 55),
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-/*
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple.shade400,
-          brightness: Brightness.dark,
-        ),
-      ),
-      home: const MyHomePage(),
+      title: 'Habit Tracker',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class Habit {
+  String activity;
+  DateTime date;
+  Habit(this.activity, this.date);
+}
+
+class _HomePageState extends State<HomePage> {
+  //this is where the data will go
+  List<Habit> habits = [];
+
+  void addHabit(Habit h) {
+    setState(() => habits.add(h));
+  }
+
+  void removeHabit(int counter) {
+    setState(() => habits.removeAt(counter));
+  }
+
+  final textController = TextEditingController();
+  //cant use const because this will not be a static
+  /*Widget NewBuildHabitTile(Habit habit, int element){
+   return ListTile()
+ }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: DataTable(
-          border: TableBorder.all(color: Colors.white),
-          headingTextStyle: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(9.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Habit',
+                        hintText: 'Enter The Habit Here!',
+                      ),
+                    ),
+                  ),
+                ],
+              )
           ),
-          columns: [
-            DataColumn(
-              label: ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: 150),
-                child: const Text("Habit",
-                    textAlign: TextAlign.center,
-                ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: habits.length,
+              itemBuilder: (context, index) {
+                final habit = habits[index];
+                TextField(controller: textController);
+                return ListTile(
+                  title: Text(habit.activity),
+                  subtitle: Text(
+                    habit.date.toString(),
+                  ), //need a to string since date is a another type
+                  hoverColor: Colors.grey,
+                );
+              },
+            ),
+          ),
+        ],
       ),
-              ),
-            DataColumn(
-              label: ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: 150),
-                child: const Text("Date",
-                  textAlign: TextAlign.center,
-          ),
-        ),
-              ),
-          ],
-          rows: const [
-            DataRow(cells: [
-              DataCell(TextField()),
-              DataCell(Text("hi",
-              textAlign: TextAlign.center,
-                  textScaler: TextScaler.linear(1.5),)),
-            ]),
-          ],
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          addHabit(Habit(textController.text, DateTime.now()));
+          textController.clear();
+        },
+        label: Text("Add"),
+        icon: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.grey,
       ),
     );
   }
 }
 
-
-//class MyApp extends StatefulWidget {
-//  const MyApp({super.key});
-//
-//  @override
-//  State<MyApp> createState() => _MyAppState();
-//}
-//
-//class _MyAppState extends State<MyApp> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return const Placeholder();
-//  }
-//}
-*/
--- lib/main2.dart
+++ lib/main2.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp(const MyApp());}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,51 +17,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//class _MyAppState extends State<MyApp> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//      title: 'Flutter Demo',
-//      theme: ThemeData(
-//        colorScheme: ColorScheme.fromSeed(
-//          seedColor: Colors.deepPurple.shade400,
-//          brightness: Brightness.dark,
-//        ),
-//      ),
-//      home: const MyHomePage(),
-//    );
-//  }
-//}
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-
-
-class Habit{
+class Habit {
   String activity;
   DateTime date;
-  Habit(this.activity,this.date);
+  Habit(this.activity, this.date);
 }
 
-
 class _HomePageState extends State<HomePage> {
-//this is where the data will go
- List<Habit> habits = [];
+  //this is where the data will go
+  List<Habit> habits = [];
 
- void addHabit(Habit h){
-   setState(() => habits.add(h));
- }
- void removeHabit(int counter){
-   setState(() => habits.removeAt(counter));
- }
-final textController = TextEditingController();
- //cant use const because this will not be a static
- /*Widget NewBuildHabitTile(Habit habit, int element){
+  void addHabit(Habit h) {
+    setState(() => habits.add(h));
+  }
+
+  void removeHabit(int counter) {
+    setState(() => habits.removeAt(counter));
+  }
+
+  final textController = TextEditingController();
+  //cant use const because this will not be a static
+  /*Widget NewBuildHabitTile(Habit habit, int element){
    return ListTile()
  }*/
 
@@ -67,45 +51,54 @@ final textController = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-children: [
-  Padding(padding: EdgeInsets.all(9.0),
-    child: TextField(
-      decoration: InputDecoration(
-        labelText: 'Habit',
-        hintText: 'Enter The Habit Here!',
-         ),
-     ),
-  ),
-    Expanded(
-      child: ListView.builder(
-        itemCount: habits.length,
-        itemBuilder: (context, index) {
-    final habit = habits[index];
-    TextField(controller: textController,);
-    return ListTile(
-    title: Text(habit.activity),
-    subtitle: Text(habit.date.toString()), //need a to string since date is a another type
-    hoverColor: Colors.grey,
-    );
-    },
-    )
-    )
-    ],
+        children: [
+          Padding(
+            padding: EdgeInsets.all(9.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Habit',
+                    hintText: 'Enter The Habit Here!',
+                  ),
+                ),
+                ),
+              ],
+            )
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: habits.length,
+              itemBuilder: (context, index) {
+                final habit = habits[index];
+                TextField(controller: textController);
+                return ListTile(
+                  title: Text(habit.activity),
+                  subtitle: Text(
+                    habit.date.toString(),
+                  ), //need a to string since date is a another type
+                  hoverColor: Colors.grey,
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton.extended( onPressed: () {
-        addHabit(Habit(textController.text, DateTime.now()));
-        textController.clear();
-      },
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          addHabit(Habit(textController.text, DateTime.now()));
+          textController.clear();
+        },
         label: Text("Add"),
         icon: Icon(Icons.add),
         backgroundColor: Colors.black,
         foregroundColor: Colors.grey,
       ),
     );
-              }
-          }
-
+  }
+}
 
 /*class textfieldclass extends StatefulWidget {
   const textfieldclass({super.key});
@@ -127,14 +120,3 @@ class _textfieldclassState extends State<textfieldclass> {
     );
   }
 }*/
-
-
-
-
-
-
-
-
-
-
-
